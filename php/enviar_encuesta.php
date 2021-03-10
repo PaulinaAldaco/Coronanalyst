bb91d734b969a6357ee3997afb79e52e56492b01
Paulina Aldaco <A01635325@itesm.mx>
2021-03-10 14:45:15 -0700

    :ambulance: Se corrigió errores al mandaar valores de checkboxes a la base

--- php/enviar_encuesta.php
+++ php/enviar_encuesta.php
@@ -6,41 +6,41 @@
     $sql2 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . htmlspecialchars($_GET["compra"]) . "')";
     $sql3 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . htmlspecialchars($_GET["compra2019"]) . "')";
     $sql4 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . htmlspecialchars($_GET["compra2020"]) . "')";
-    $sql5 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . htmlspecialchars($_GET["plataforma"]) . "')";
-    $sql6 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . htmlspecialchars($_GET["categoria"]) . "')";
+    $sql5 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . implode(",",$_GET["plataforma"] . "')";
+    $sql6 = "INSERT INTO Respuestas (ID_pregunta, ID_usuario, respuesta) VALUES (1,1,'" . implode(",",$_GET["categoria"] . "')";
 
     if ($conn->query($sql1) === TRUE) {
-      echo "Registro 1 agregado";
+      echo "Confianza: ". htmlspecialchars($_GET["confianza"]) . "<br>";
     } else {
       echo "Error: " . $sql1 . "<br>" . $conn->error;
     }
 
     if ($conn->query($sql2) === TRUE) {
-      echo "Registro 2 agregado";
+      echo "Compra: ". htmlspecialchars($_GET["compra"]) . "<br>";
     } else {
       echo "Error: " . $sql2 . "<br>" . $conn->error;
     }
 
     if ($conn->query($sql3) === TRUE) {
-      echo "Registro 3 agregado";
+      echo "Compra 2019: ". htmlspecialchars($_GET["compra2019"]) . "<br>";
     } else {
       echo "Error: " . $sq3 . "<br>" . $conn->error;
     }
 
     if ($conn->query($sql4) === TRUE) {
-      echo "Registro 4 agregado";
+      echo "Compra 2020: ". htmlspecialchars($_GET["compra2020"]) . "<br>";
     } else {
       echo "Error: " . $sql4 . "<br>" . $conn->error;
     }
 
     if ($conn->query($sql5) === TRUE) {
-      echo "Registro 5 agregado";
+      echo "Plataformas: ". implode(",",$_GET["plataforma"] . "<br>";
     } else {
       echo "Error: " . $sql5 . "<br>" . $conn->error;
     }
 
     if ($conn->query($sql6) === TRUE) {
-      echo "Registro 6 agregado";
+      echo "Categorias: ". implode(",",$_GET["plataforma"] . "<br>";
     } else {
       echo "Error: " . $sql6 . "<br>" . $conn->error;
     }
