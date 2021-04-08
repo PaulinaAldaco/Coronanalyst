27c1f2671dc8e59a7ad7ba14527ddbfaa6752c91
Aralí Mata <aralipdf@gmail.com>
2021-04-08 09:23:51 -0600

    login modificado

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,9 @@
 import './Login.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import logo from './imagenes/logo_coronanalyst.jpeg'
 
-function Login() {
+/*function Login() {
   return (
    
     <div>
@@ -32,7 +33,39 @@ function Login() {
     <Footer/>
     </div>
   );
+}*/
+
+function Login() {
+  return (
+   
+    <div>
+    <Header/>
+    <div class="split left">
+        <div class="centered">
+        <h1>¡Bienvenido!</h1>
+        <p>Introduce tu correo y contraseña para acceder</p>
+        <form>
+
+            <input type="text" id = "email" placeholder="Correo electrónico" name="email" required />
+            <input type="password" id = "contra" placeholder="Contraseña" name="contra" required />
+            
+            <input type = "submit" value="Entrar"/>
+        </form>
+        <b><p><a href="#" id="ref">¿No tienes cuenta? Registrate aquí</a></p></b>
+        </div>
+    </div>
+
+    <div class="split right">
+        <div class="centered">
+        <img src={logo} alt="Logo coronanalyst"/>
+        <button>Regresar a inicio</button>
+        </div>
+    </div>
+    <Footer/>
+    </div>
+  );
 }
+
 
 
 export default Login;
