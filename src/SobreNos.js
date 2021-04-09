0efe79884256ed11337fabe6531ba963f75b7284
salvador2307 <77424583+salvador2307@users.noreply.github.com>
2021-04-09 17:24:53 -0600

    Creación de inicio y pantalla sobre nosotros

---
+++ src/SobreNos.js
@@ -0,0 +1,43 @@
+import './Login.css';
+import './sobreNos.css'
+import logo from "./Logo_blue_desert.jpeg"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function SobreNos(){ 
+    return(
+        <section id = "main-content">
+            <Header/>
+            <form action = "php/enviar_consulta.php">
+                <h1>¿Qué es Blue Desert? </h1>
+
+                <div class="bloque">
+                    <div class="b1">
+                        <p> A  finales de 2019 comenzó la pandemia actual de COVID-19, resultando en una cuarentena global desde marzo del 2020. Esto ha tenido repercusiones profundas en todos los ámbitos de la vida humana, incluyendo, naturalmente, el sector de salud pública, pero también el empresarial y económico. Así mismo, la pandemia ha ocasionado fuertes alteraciones a la vida cotidiana de las personas. Un tema importante es el impacto sobre el comportamiento de compra, ya que el estado de cuarentena ha presentado nuevas dificultades para muchos para llevar a cabo sus compras, tanto las relacionadas a comodidades como las de necesidades básicas. Por esto mismo, la pandemia ha resultado perjudicial para la economía de grandes y pequeñas empresas. En este contexto, es de interés económico y social analizar las nuevas tendencias (en contraste con las anteriores a la pandemia) en el comportamiento de compra del público.
+                        </p>
+                        <p>
+                            Es por esto que Blue Desert es el desarrollador de "Coronanalyst", un sistema que tiene como finalidad identificar los cambios, afectaciones y nuevas tendencias en el estilo de vida de las personas a raíz de la actual pandemia. El recabar esta información ayudará a diferentes industrias a adaptarse y poder ofrecer productos y servicios de una mayor calidad y enfocarse principalmente en las necesidades del cliente.
+                        </p>
+                    </div>
+                    <div class="b1">
+                        <img class="logo" src={logo}/>  
+                    </div>
+                
+                </div>
+
+                
+                
+                
+                
+                
+            </form>
+
+            <Footer/>
+        </section>
+         
+    )
+
+   
+}
+
+export default SobreNos;
---
+++ src/inicio.js
@@ -0,0 +1,49 @@
+import './Login.css';
+import './inicio.css'
+import grafica from "./graficas.png"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function inicio(){ 
+    return(
+        <section id = "main-content">
+            <Header/>
+            <form action = "php/enviar_consulta.php">
+                <main>
+                    <h1>Analizando los efectos del COVID-19 </h1>
+
+                    <div class="bloque">
+                        <div class="b1">
+                            <img class="logo" src={grafica}/>     
+                        </div>
+                        <div class="b2">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>   
+                            <button type="button">Contestar encuesta</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p class="texto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>   
+                    </div>
+
+                </main>
+                
+
+                
+
+                
+                
+                
+                
+                
+            </form>
+
+            <Footer/>
+        </section>
+         
+    )
+
+   
+}
+
+export default inicio;
