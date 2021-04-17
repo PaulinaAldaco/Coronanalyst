a47ed122db458c73dadc6fd9dbe2ee2304ce2de9
Aralí Mata <aralipdf@gmail.com>
2021-04-17 14:41:21 -0600

    :art: Registro y sesión cerrada igual a login

--- api/login.php
+++ api/login.php
@@ -1,9 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: access, Content-Type, Authorization, X-Requested-With ");
+header("Access-Control-Allow-Methods: POST");
+
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 function msg($success,$status,$message,$extra = []){
     return array_merge([
